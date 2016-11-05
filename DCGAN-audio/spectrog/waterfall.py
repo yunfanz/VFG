6d164219991ed91c8f71bd07b33fba98c8e899f8
Yunfan Zhang <yunfanz@airbears2-10-142-186-143.airbears2.1918.berkeley.edu>
2016-11-04 22:20:19 -0700

    add axes labels

--- DCGAN-audio/spectrog/waterfall.py
+++ DCGAN-audio/spectrog/waterfall.py
@@ -1,4 +1,4 @@
-import numpy as n, pylab as plt
+import numpy as n, matplotlib.pyplot as plt
 import sys, scipy
 from mpl_toolkits.basemap import Basemap
 def data_mode(data, mode='abs'):
@@ -13,14 +13,19 @@ def data_mode(data, mode='abs'):
         data = n.log10(data)
     else: raise ValueError('Unrecognized plot mode.')
     return data
-def waterfall(d, ax=None, mode='log', mx=None, drng=None, recenter=False, **kwargs):
+def waterfall(d, ax=None, mode='log', mx=None, drng=None, **kwargs):
     if n.ma.isMaskedArray(d): d = d.filled(0)
-    if recenter: d = a.img.recenter(d, n.array(d.shape)/2)
     d = data_mode(d, mode=mode)
     if mx is None: mx = d.max()
     if drng is None: drng = mx - d.min()
     mn = mx - drng
     if ax:
-    	return ax.imshow(d, vmax=mx, vmin=mn, aspect='auto', interpolation='nearest', **kwargs)
+    	im = ax.imshow(d, vmax=mx, vmin=mn, aspect='auto', interpolation='nearest', **kwargs)
+        ax.set_xlabel('Frequency')
+        ax.set_ylabel('Time')
     else:
-    	return plt.imshow(d, vmax=mx, vmin=mn, aspect='auto', interpolation='nearest', **kwargs)
+    	im = plt.imshow(d, vmax=mx, vmin=mn, aspect='auto', interpolation='nearest', **kwargs)
+        plt.xlabel('Frequency')
+        plt.ylabel('Time')
+    return im
+
